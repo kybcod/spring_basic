3ad6f1bdf4cd079c2bc8478a76d531f967c35da4
kyb <kyb-0215@naver.com>
2024-11-26 22:19:24 +0900

    @RequestParam과 @ModelAttribute

--- src/main/java/com/fastcampus/ch2/YoilTellerMVC.java
+++ src/main/java/com/fastcampus/ch2/YoilTellerMVC.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 // 년월일을 입력하면 요일을 알려주는 프로그램
 @Controller
 public class YoilTellerMVC {
+	
 	@RequestMapping("/getYoilMVC")
     //public static void main(HttpServletRequest request, HttpServletResponse response) throws IOException {
 	public String main(int year, int month, int day, Model model) throws IOException {
